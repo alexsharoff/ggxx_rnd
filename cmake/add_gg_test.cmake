e24367fd701b165b97d99a0619bdc71a35c2d18c
Alex Sharoff <707030+alexsharoff@users.noreply.github.com>
2020-04-18 03:15:32 +0700

    add_gg_test: cmake function for replay-based tests

---
+++ cmake/add_gg_test.cmake
@@ -0,0 +1,19 @@
+function(add_gg_test NAME)
+    cmake_parse_arguments(
+        PARSE_ARGV 1 _ "" "TIMEOUT" "ARGS"
+    )
+
+    set(test_name gg_${NAME})
+
+    add_test(NAME ${test_name}
+        COMMAND gg --replay ${CMAKE_SOURCE_DIR}/replays/${NAME}.ggr ${_ARGS}
+    )
+
+    set(timeout 60)
+    if (_TIMEOUT)
+        set(timeout ${_TIMEOUT})
+    endif()
+    set_tests_properties(${test_name} PROPERTIES
+        TIMEOUT ${timeout}
+    )
+endfunction()
