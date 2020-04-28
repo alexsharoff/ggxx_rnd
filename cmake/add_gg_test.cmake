33331a0317af2445a098ea0977adb29be8c29366
Alex Sharoff <707030+alexsharoff@users.noreply.github.com>
2020-04-29 01:17:02 +0700

    test framework reorganized

--- cmake/add_gg_test.cmake
+++ cmake/add_gg_test.cmake
@@ -1,17 +1,62 @@
-# add_gg_test(NAME [ARGS args...] [TIMEOUT seconds] [DISABLED]
-# NAME: path to test replay is based on test name: <SRC_DIR>/replays/<NAME>.ggr
+if(NOT DEFINED LIBGG_TEST_TIMEOUT)
+    set(LIBGG_TEST_TIMEOUT 60)
+endif()
+if(NOT DEFINED LIBGG_TEST_RUN_SERIAL)
+    set(LIBGG_TEST_RUN_SERIAL OFF)
+endif()
+if(NOT DEFINED LIBGG_TEST_NOGRAPHICS)
+    set(LIBGG_TEST_NOGRAPHICS ON)
+endif()
+if(NOT DEFINED LIBGG_TEST_NOSOUND)
+    # Not supported yet
+    set(LIBGG_TEST_NOSOUND OFF)
+endif()
+if(NOT DEFINED LIBGG_TEST_UPDATE_STATE_CHECKSUM)
+    set(LIBGG_TEST_UPDATE_STATE_CHECKSUM OFF)
+endif()
+if(NOT DEFINED LIBGG_TEST_REPLAY_DIR)
+    set(LIBGG_TEST_REPLAY_DIR "${CMAKE_SOURCE_DIR}/test/replays")
+endif()
+
+# add_gg_test(REPLAY path [NAME name ] [TIMEOUT seconds] [DISABLED] [RUN_SERIAL] [ARGS args...]
+# REPLAY path relative to LIBGG_TEST_REPLAY_DIR (${CMAKE_SOURCE_DIR}/test/replays)
+# NAME: test name
 # ARGS: arguments for gg.exe / GGXXACPR_Win.exe. For full list of supported arguments,
 #       run gg.exe --help.
-function(add_gg_test NAME)
+# DISABLED: skip test
+# TIMEOUT: test timeout in seconds. Default: LIBGG_TEST_TIMEOUT (60)
+# RUN_SERIAL: don't run other test simultaneously with this one. Default: LIBGG_TEST_RUN_SERIAL (OFF)
+function(add_gg_test)
     cmake_parse_arguments(
-        PARSE_ARGV 1 "" "DISABLED" "TIMEOUT" "ARGS"
+        PARSE_ARGV 0 "" "DISABLED;RUN_SERIAL" "NAME;REPLAY;TIMEOUT" "ARGS"
     )
 
     if(_DISABLED)
         return()
     endif()
 
-    set(test_name gg_${NAME})
+    if(NOT _REPLAY)
+        message(FATAL_ERROR "REPLAY is not specified or invalid")
+    endif()
+
+    if(NOT IS_ABSOLUTE ${_REPLAY})
+        set(_REPLAY ${LIBGG_TEST_REPLAY_DIR}/${_REPLAY})
+    endif()
+
+    if(NOT EXISTS "${_REPLAY}")
+        message(FATAL_ERROR "File does not exist: ${_REPLAY}")
+    endif()
+
+    if(NOT _NAME)
+        get_filename_component(_NAME ${_REPLAY} NAME_WE)
+    endif()
+
+    foreach(var _TIMEOUT _RUN_SERIAL)
+        # set default value if unspecified
+        if(NOT DEFINED ${var})
+            set(${var} ${LIBGG_TEST${var}})
+        endif()
+    endforeach()
 
     if(LIBGG_TEST_UPDATE_STATE_CHECKSUM)
         # Game state has been extended, thus exactly the same frame
@@ -27,21 +72,11 @@ function(add_gg_test NAME)
     endif()
     list(APPEND _ARGS --noinput)
 
-    add_test(NAME ${test_name}
-        COMMAND gg --replay ${CMAKE_SOURCE_DIR}/replays/${NAME}.ggr ${_ARGS}
-    )
+    set(command gg ${_REPLAY} ${_ARGS})
+    add_test(NAME ${_NAME} COMMAND ${command})
 
-    set(timeout 60)
-    if (_TIMEOUT)
-        set(timeout ${_TIMEOUT})
-    endif()
-    set_tests_properties(${test_name} PROPERTIES
-        TIMEOUT ${timeout}
+    set_tests_properties(${_NAME} PROPERTIES
+        TIMEOUT ${_TIMEOUT}
+        RUN_SERIAL ${_RUN_SERIAL}
     )
-
-    if(LIBGG_TEST_RUN_SERIAL)
-        set_tests_properties(${test_name} PROPERTIES
-            RUN_SERIAL ON
-        )
-    endif()
 endfunction()
---
+++ test/gg.cmake
@@ -0,0 +1,40 @@
+include(add_gg_test)
+
+# Enable once [!] after game state has been extended and all autotests started to fail
+set(LIBGG_TEST_UPDATE_STATE_CHECKSUM OFF)
+
+add_gg_test(
+    REPLAY bugrepro/fafnir_tyrant_rave.ggr
+    ARGS --gamemode network --checkstate --synctest 8
+)
+add_gg_test(
+    REPLAY bugrepro/kliff_throw.ggr
+    ARGS --gamemode network --checkstate --synctest 1
+)
+add_gg_test(
+    REPLAY bugrepro/faust_taunt.ggr
+    ARGS
+        --checkstate
+        --rollback 1400 1050
+        --rollback 1460 1050
+)
+add_gg_test(
+    REPLAY bugrepro/kliff_taunt.ggr
+    ARGS
+        --checkstate
+        --rollback 840 691
+        --rollback 1289 840
+        --rollback 1300 1288
+)
+add_gg_test(
+    REPLAY bugrepro/charselect_screen.ggr
+    ARGS --gamemode network --checkstate --synctest 1
+)
+add_gg_test(
+    REPLAY bugrepro/charselect_screen_random_char.ggr
+    ARGS --gamemode network --checkstate --synctest 1
+)
+add_gg_test(
+    REPLAY bugrepro/charselect_screen_random_stage.ggr
+    ARGS --gamemode network --checkstate --synctest 1
+)
