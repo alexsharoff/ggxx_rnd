6ae3c55d231d5178327ed874f4c5871a320d10ca
Alex Sharoff <707030+alexsharoff@users.noreply.github.com>
2020-05-06 04:17:41 +0700

    don't steal focus from current window when starting with /unattended

--- src/unattended.cpp
+++ src/unattended.cpp
@@ -11,6 +11,9 @@ void Initialize(IGame* game, configuration* cfg)
 {
     if (cfg->get_args().unattended)
     {
+        constexpr uint8_t jmp[] = { 0xe9, 0x38, 0x01, 0x00, 0x00 };
+        memory_dump::dump(jmp, game->GetImageBase() + 0x1473D7);
+
         constexpr uint8_t showwindow_cmd = SW_SHOWMINNOACTIVE;
         memory_dump::dump(showwindow_cmd, game->GetImageBase() + 0x1473B6);
 
