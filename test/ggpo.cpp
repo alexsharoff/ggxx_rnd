c5a8aafd51a0ce689de1568e56a6af3a52e6c28f
Alex Sharoff <707030+alexsharoff@users.noreply.github.com>
2020-05-25 04:16:25 +0700

    ggpo test: first version

--- test/ggpo.cpp
+++ test/ggpo.cpp
@@ -6,12 +6,15 @@
 #include <cstdint>
 #include <functional>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
 
 #include <Windows.h>
 #include <timeapi.h>
 #include <Winsock2.h>
+
+#include "test.h"
 
 
 #define GGPO_CHECK(expr) \
@@ -24,32 +27,35 @@
     } while(false)
 
 
-using input_t = std::array<uint8_t, 4>;
+using input_t = std::array<int8_t, 4>;
+using inputs_vector_t = std::vector<std::array<input_t, 2>>;
 
-class GameState
+class TestGame
 {
 public:
-    GameState(int side, const std::vector<input_t>& input, uint16_t localport, const std::string_view& remoteip, uint16_t remoteport)
+    TestGame(int side, const inputs_vector_t& inputs, uint16_t localport, const std::string_view& remoteip, uint16_t remoteport)
     {
         GGPOSessionCallbacks callbacks;
-        if (side == 1)
-        {
-            callbacks.advance_frame = Callbacks::advance_frame_p1;
-            callbacks.on_event = Callbacks::on_event_p1;
-            callbacks.load_game_state = Callbacks::load_game_state_p1;
-            callbacks.save_game_state = Callbacks::save_game_state_p1;
-        }
-        else
+        if (side == 2)
         {
             callbacks.advance_frame = Callbacks::advance_frame_p2;
             callbacks.on_event = Callbacks::on_event_p2;
             callbacks.load_game_state = Callbacks::load_game_state_p2;
             callbacks.save_game_state = Callbacks::save_game_state_p2;
+            Callbacks::g_game_state_p2 = this;
+        }
+        else
+        {
+            callbacks.advance_frame = Callbacks::advance_frame_p1;
+            callbacks.on_event = Callbacks::on_event_p1;
+            callbacks.load_game_state = Callbacks::load_game_state_p1;
+            callbacks.save_game_state = Callbacks::save_game_state_p1;
+            Callbacks::g_game_state_p1 = this;
         }
         callbacks.free_buffer = Callbacks::free_buffer;
         callbacks.begin_game = Callbacks::begin_game;
         callbacks.log_game_state = Callbacks::log_game_state;
-        GGPO_CHECK(ggpo_start_session(&m_session, &callbacks, "TEST", 2, 2, localport));
+        GGPO_CHECK(ggpo_start_session(&m_session, &callbacks, "TEST", 2, 4, localport));
 
         for (int player_num = 1; player_num <= 2; ++player_num)
         {
@@ -70,64 +76,92 @@ public:
             GGPO_CHECK(ggpo_add_player(m_session, &player, &m_player_handles[player_num-1]));
         }
 
-        m_input = input;
+        m_frame_inputs = inputs;
         m_side = side;
     }
 
-    ~GameState()
+    ~TestGame()
     {
         ggpo_close_session(m_session);
     }
 
-    bool advance_frame(const input_t input[2], int disconnect_flags)
+    bool advance_frame(const std::array<input_t, 2>& inputs, int /* disconnect_flags */)
     {
-        (void)input;
-        (void)disconnect_flags;
+        for (const auto& input : inputs)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                if (input[i] < 0)
+                    --m_state.text[i];
+                if (input[i] > 0)
+                    ++m_state.text[i];
+            }
+        }
+        ++m_state.frame;
         ggpo_advance_frame(m_session);
         return true;
     }
 
-    bool save_game_state(unsigned char** buffer, int* len , int* checksum, int /* frame */) const
+    bool save_game_state(unsigned char** buffer, int* len , int* checksum, int frame) const
     {
-        *buffer = new unsigned char[4];
-        const char* test = "test";
-        std::copy(test, test + 4, *buffer);
-        *len = 4;
-        *checksum = reinterpret_cast<int>(this);
-
+        (void)frame;
+        assert(static_cast<size_t>(frame) == m_state.frame);
+        *checksum = *reinterpret_cast<const int*>(m_state.text);
+        *buffer = new unsigned char[sizeof(m_state)];
+        auto data = reinterpret_cast<const unsigned char*>(&m_state);
+        std::copy(data, data + sizeof(m_state), *buffer);
+        *len = sizeof(m_state);
         return true;
     }
 
     static void free_buffer(void* buffer)
     {
+        auto p = (char*)buffer;
         delete[] buffer;
+        std::memset(p, -1, 12);
     }
 
-    bool load_game_state(unsigned char* /* buffer */, int /* len */)
+    bool load_game_state(unsigned char* buffer, int len)
     {
+        (void)len;
+        assert(sizeof(m_state) == len);
+        auto data = reinterpret_cast<unsigned char*>(&m_state);
+        std::copy(buffer, buffer + sizeof(m_state), data);
         return true;
     }
 
-    bool on_event(GGPOEvent* info)
+    bool on_event(GGPOEvent* event)
     {
-        switch (info->code)
+        std::lock_guard<std::mutex> guard(s_stdout_mutex);
+        print_log_prefix();
+        int sync_progress = 0;
+        switch (event->code)
         {
         case GGPO_EVENTCODE_CONNECTED_TO_PEER:
+            std::cout << "Connected" << std::endl;
             break;
         case GGPO_EVENTCODE_SYNCHRONIZING_WITH_PEER:
+            sync_progress = event->u.synchronizing.count * 100 / event->u.synchronizing.total;
+            std::cout << "Synchronizing: " << sync_progress << '%' << std::endl;
             break;
         case GGPO_EVENTCODE_SYNCHRONIZED_WITH_PEER:
+            std::cout << "Synchronizing: done" << std::endl;
             break;
         case GGPO_EVENTCODE_RUNNING:
+            std::cout << "Running" << std::endl;
             break;
         case GGPO_EVENTCODE_CONNECTION_INTERRUPTED:
+            std::cout << "Connection interrupted" << std::endl;
             break;
         case GGPO_EVENTCODE_CONNECTION_RESUMED:
+            std::cout << "Connection resumed" << std::endl;
             break;
         case GGPO_EVENTCODE_DISCONNECTED_FROM_PEER:
+            std::cout << "Disconnected" << std::endl;
             break;
         case GGPO_EVENTCODE_TIMESYNC:
-            m_timesync_frames = info->u.timesync.frames_ahead;
+            std::cout << "Timesync: " << event->u.timesync.frames_ahead << " frames ahead" << std::endl;
+            m_frames_ahead = event->u.timesync.frames_ahead;
             break;
         }
         return true;
@@ -135,7 +169,7 @@ public:
 
     bool input_ended() const
     {
-        return m_input.size() <= m_input_idx;
+        return m_frame_inputs.size() <= m_state.frame;
     }
 
     void idle(uint64_t timeout_ms) const
@@ -145,32 +179,72 @@ public:
 
     void run_frame()
     {
-        auto local_input = m_input[m_input_idx++];
-        auto result = ggpo_add_local_input(
-            m_session, m_player_handles[m_side-1], &local_input, sizeof(local_input)
-        );
-        if (GGPO_SUCCEEDED(result))
+        if (!m_frames_ahead)
         {
-            int disconnect_flags;
-            input_t inputs[2];
-            result = ggpo_synchronize_input(m_session, inputs, sizeof(inputs), &disconnect_flags);
+            GGPOErrorCode result = GGPO_OK;
+            auto frame_inputs = m_frame_inputs[m_state.frame];
+            for (int side = 1; side <= 2; ++side)
+            {
+                if (m_side == 0 || m_side == side)
+                {
+                    auto& input = frame_inputs[side-1];
+                    result = ggpo_add_local_input(
+                        m_session, m_player_handles[side-1], &input, sizeof(input)
+                    );
+                }
+            }
             if (GGPO_SUCCEEDED(result))
-                advance_frame(inputs, disconnect_flags);
+            {
+                int disconnect_flags;
+                std::array<input_t, 2> inputs;
+                result = ggpo_synchronize_input(m_session, &inputs, sizeof(inputs), &disconnect_flags);
+                if (GGPO_SUCCEEDED(result))
+                {
+                    advance_frame(inputs, disconnect_flags);
+                }
+            }
+        }
+        else
+        {
+            std::lock_guard<std::mutex> guard(s_stdout_mutex);
+            print_log_prefix();
+            std::cout << "Timesync" << std::endl;
+            --m_frames_ahead;
         }
         draw_current_frame();
     }
 
     void draw_current_frame() const
     {
+        std::lock_guard<std::mutex> guard(s_stdout_mutex);
+        print_log_prefix();
+        std::cout << m_state.text << std::endl;
+    }
+
+    void print_log_prefix() const
+    {
+        std::cout << 'f' << m_state.frame << " s" << m_side << ": ";
+    }
+
+    struct GameState
+    {
+        size_t frame = 0;
+        char text[5] = "TEST";
+    } m_state;
+
+    const GameState& state() const
+    {
+        return m_state;
     }
 
 private:
+    static std::mutex s_stdout_mutex;
+
     GGPOSession* m_session;
     GGPOPlayerHandle m_player_handles[2];
     int m_side;
-    int m_timesync_frames = 0;
-    std::vector<input_t> m_input;
-    size_t m_input_idx = 0;
+    int m_frames_ahead = 0;
+    inputs_vector_t m_frame_inputs;
 
     GGPOSession* get_session() const
     {
@@ -179,14 +253,14 @@ private:
 
     struct Callbacks
     {
-        static GameState* g_game_state_p1;
-        static GameState* g_game_state_p2;
+        static TestGame* g_game_state_p1;
+        static TestGame* g_game_state_p2;
         static bool begin_game(const char*)
         {
             return true;
         }
 
-        static bool save_game_state(GameState& game, unsigned char** buffer, int* len, int* checksum, int frame)
+        static bool save_game_state(TestGame& game, unsigned char** buffer, int* len, int* checksum, int frame)
         {
             return game.save_game_state(buffer, len, checksum, frame);
         }
@@ -201,7 +275,7 @@ private:
             return save_game_state(*g_game_state_p2, buffer, len, checksum, frame);
         }
 
-        static bool load_game_state(GameState& game, unsigned char* buffer, int len)
+        static bool load_game_state(TestGame& game, unsigned char* buffer, int len)
         {
             return game.load_game_state(buffer, len);
         }
@@ -223,13 +297,13 @@ private:
 
         static void free_buffer(void* buffer)
         {
-            GameState::free_buffer(buffer);
+            TestGame::free_buffer(buffer);
         }
 
-        static bool advance_frame(GameState& game)
+        static bool advance_frame(TestGame& game)
         {
             int disconnect_flags = 0;
-            input_t inputs[2];
+            std::array<input_t, 2> inputs;
             ggpo_synchronize_input(game.get_session(), &inputs, sizeof(inputs), &disconnect_flags);
             return game.advance_frame(inputs, disconnect_flags);
         }
@@ -244,7 +318,7 @@ private:
             return advance_frame(*g_game_state_p2);
         }
 
-        static bool on_event(GameState& game, GGPOEvent* event)
+        static bool on_event(TestGame& game, GGPOEvent* event)
         {
             return game.on_event(event);
         }
@@ -261,23 +335,12 @@ private:
     };
 };
 
-GameState* GameState::Callbacks::g_game_state_p1 = nullptr;
-GameState* GameState::Callbacks::g_game_state_p2 = nullptr;
+TestGame* TestGame::Callbacks::g_game_state_p1 = nullptr;
+TestGame* TestGame::Callbacks::g_game_state_p2 = nullptr;
+std::mutex TestGame::s_stdout_mutex;
 
-struct Settings
+void game_loop(TestGame& game, uint32_t fps)
 {
-    int side;
-    std::vector<input_t> input;
-    uint16_t localport;
-    const std::string remoteip = "127.0.0.1";
-    uint16_t remoteport;
-    int fps = 60;
-};
-
-void test_loop(const Settings& settings)
-{
-    GameState game(settings.side, settings.input, settings.localport, settings.remoteip, settings.remoteport);
-
     using std::chrono::high_resolution_clock;
     auto next = high_resolution_clock::now();
     for (;;)
@@ -290,7 +353,7 @@ void test_loop(const Settings& settings)
         if (now >= next)
         {
             game.run_frame();
-            next = now + std::chrono::seconds(1) / settings.fps;
+            next = now + std::chrono::seconds(1) / fps;
         }
     }
 }
@@ -324,25 +387,25 @@ private:
 int main()
 {
     GlobalInit init(1);
-    Settings settings_p1, settings_p2;
 
-    settings_p1.side = 1;
-    settings_p1.input = {{}};
-    settings_p1.localport = 11111;
-    settings_p1.remoteport = 22222;
-
-    settings_p2.side = 2;
-    settings_p2.input = {{}};
-    settings_p2.localport = 22222;
-    settings_p2.remoteport = 11111;
+    inputs_vector_t inputs(60);
+    inputs[28][0][0] = -1;
+    inputs[28][1][0] = 1;
+    inputs[28][1][1] = 1;
+    uint16_t port_p1 = 11111;
+    uint16_t port_p2 = 22222;
+    TestGame game_p1(1, inputs, port_p1, "127.0.0.1", port_p2);
+    TestGame game_p2(2, inputs, port_p2, "127.0.0.1", port_p1);
 
     std::thread threads[] = {
-        std::thread(test_loop, settings_p1),
-        std::thread(test_loop, settings_p2)
+        std::thread(game_loop, std::ref(game_p1), 60),
+        std::thread(game_loop, std::ref(game_p2), 60)
     };
 
     for (auto& t : threads)
         t.join();
+
+    TEST_EQ(std::string_view(game_p1.state().text), game_p2.state().text);
 
     return 0;
 }
