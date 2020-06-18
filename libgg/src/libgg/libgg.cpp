71c0ccdffbbf52f593adc8713321844d69b4d55b
Alex Sharoff <707030+alexsharoff@users.noreply.github.com>
2020-06-19 02:34:18 +0700

    show message if libgg.dll failed to initialize

--- libgg/src/libgg/libgg.cpp
+++ libgg/src/libgg/libgg.cpp
@@ -12,6 +12,7 @@
 #include "training_mode_ex.h"
 #include "skip_intro.h"
 #include "sound_fix.h"
+#include "ui.h"
 #include "unattended.h"
 
 
@@ -28,24 +29,39 @@ extern "C" __declspec(dllexport) bool libgg_init()
 
         const auto image_base = (size_t)::GetModuleHandle(nullptr);
         if (!apply_patches(image_base))
+        {
+            show_message_box(L"Unable to modify process memory.", true);
             return false;
+        }
+
         s_game = IGame::Initialize(image_base, s_cfg.get());
         if (!s_game)
+        {
+            show_message_box(L"Unable to create IGame object.", true);
             return false;
+        }
+
         if (!unattended::Initialize(s_game.get(), s_cfg.get()))
-            return false;
+            show_message_box(L"Extension error: unattended", true);
+
         if (!print_state::Initialize(s_game.get(), s_cfg.get()))
-            return false;
+            show_message_box(L"Extension error: print_state", true);
+
         if (!ggpo::Initialize(s_game.get(), s_cfg.get()))
-            return false;
+            show_message_box(L"Extension error: ggpo", true);
+
         if (!recorder::Initialize(s_game.get(), s_cfg.get()))
-            return false;
+            show_message_box(L"Extension error: recorder", true);
+
         if (!training_mode_ex::Initialize(s_game.get(), s_cfg.get()))
-            return false;
+            show_message_box(L"Extension error: training_mode_ex", true);
+
         if (!skip_intro::Initialize(s_game.get(), s_cfg.get()))
-            return false;
+            show_message_box(L"Extension error: skip_intro", true);
+
         if (!sound_fix::Initialize(s_game.get(), s_cfg.get()))
-            return false;
+            show_message_box(L"Extension error: sound_fix", true);
+
         return true;
     }
     return false;
